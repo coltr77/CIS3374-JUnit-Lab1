c981212f0df24960815b520b2b51bc8749df6d3e
Colton Howard <coltr.howard7@gmail.com>
2017-03-07 18:04:19 -0500

    Mhyasdfasdf

---
+++ Lab1/test/com/lab1/calculator/CalculatorTest.java
@@ -0,0 +1,46 @@
+package com.lab1.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class CalculatorTest {
+
+	private Calculator calculator;
+	
+	@Before
+	public void setUp(){
+		calculator = new Calculator();
+		
+	}
+	@Test
+	public void addTest(){
+		assertEquals(4, calculator.add(2,2));
+		
+	}
+	@Test
+	public void subtractTest(){
+		assertEquals(4, calculator.subtract(6,2));
+		
+	}
+	@Test
+	public void multiplyTest(){
+		assertEquals(4, calculator.multiply(2,2));
+		
+	}
+	@Test
+	public void divideTest(){
+		assertEquals(4, calculator.divide(8,2));
+		
+	}
+	
+	@Test
+	public void isEqualTest(){
+		assertTrue(calculator.isEqual(2,2));
+		assertFalse(calculator.isEqual(2,3));
+	}
+	
+	
+
+}
